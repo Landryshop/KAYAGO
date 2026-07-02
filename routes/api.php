73888843d87d1f624248238a886773d0c4886467
Landryshop <jfalmeida500@gmail.com>
2026-07-02 12:39:11 +0100

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\DeliveryController;
+use App\Http\Controllers\Admin\AdminDeliveryController;
+
+// Routes pour l'application mobile (utilisateur)
+Route::get('/delivery/status/{trackingNumber}', [DeliveryController::class, 'getStatus']);
+
+// Routes pour le panneau Admin (gestionnaire)
+Route::post('/admin/delivery/estimate', [AdminDeliveryController::class, 'estimate']);
